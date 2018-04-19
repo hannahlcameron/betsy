98800088c310425c474f8d135cac16b2ae54dee6
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-19 15:33:29 -0700

    woring on testing

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,9 +1,40 @@
 require "test_helper"
 
 describe OrderItem do
-  let(:order_item) { OrderItem.new }
+  describe 'validation' do
+    it 'is valid with a index_reviews_on_product_id' do
 
-  it "must be valid" do
-    value(order_item).must_be :valid?
+      product_id = Product.first.id
+      order_item = OrderItem.new
+      order_item.valid?.must_equal false
+      order_item.product_id.push(product_id)
+
+    end
+
+    it 'is invalid without a product_id' do
+
+
+    end
+
+    it 'is valid with an order_id' do
+
+    end
+
+    it 'is valid without an order_id' do
+
+    end
+
+    it 'is valid with a quantity greater than zero' do
+
+    end
+
+    it 'is invalid when the quantity is not an integer' do
+
+    end
+
+    it 'is invalid when the quantity is zero' do
+
+    end
+
   end
 end
