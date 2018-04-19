8b380930d5f4c91aae8dd0f7b38550ce29fa82d7
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-19 15:22:00 -0700

    added in order model testing

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,17 +1,33 @@
 require "test_helper"
 
 describe Order do
-  it 'is valid with one order item' do
+  describe 'validations' do
+    it 'is valid with one order item' do
+      order_item = OrderItem.first
+      order = Order.new
+      order.valid?.must_equal false
+      order.order_items.push(order_item)
 
-  end
+      order.valid?.must_equal true
 
-  it 'is valid with multiple order items' do
+    end
 
-  end
+    it 'is valid with multiple order items' do
+      order_item_1 = OrderItem.first
+      order_item_2 = OrderItem.last
+      order = Order.new
+      order.valid?.must_equal false
+      order.order_items.push(order_item_1, order_item_2)
 
-  it 'is not valid with 0 order items' do
+      order.valid?.must_equal true
+    end
 
-  end
+    it 'is not valid with 0 order items' do
+      order = Order.new
+      order.valid?.must_equal false
+
+      order.errors.messages.must_include :order_items
+    end
 
   end
 end
