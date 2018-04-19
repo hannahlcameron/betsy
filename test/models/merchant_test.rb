b1da66c6e5a7b1ede093437778926db507d9d48e
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-19 14:13:43 -0700

    Test Merchant model validations

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,9 +1,44 @@
 require "test_helper"
 
 describe Merchant do
-  let(:merchant) { Merchant.new }
+  describe "validations" do
 
-  it "must be valid" do
-    value(merchant).must_be :valid?
-  end
+    it "must have a unique username" do
+      # arrange
+      merchant = Merchant.first
+      old_merchant_count = Merchant.count
+
+      new_merchant = Merchant.new(username: merchant.username, email: "address@example.com")
+
+      # assumptions
+      new_merchant.wont_be :valid?
+
+      # act
+      new_merchant.save
+
+      # assert
+      Merchant.count.must_equal old_merchant_count
+    end
+
+    it "must have a unique email" do
+      # arrange
+      merchant = Merchant.first
+      old_merchant_count = Merchant.count
+
+      new_merchant = Merchant.new(username: "example_username", email: merchant.email)
+
+      # assumptions
+      new_merchant.wont_be :valid?
+
+      # act
+      new_merchant.save
+
+      # assert
+      Merchant.count.must_equal old_merchant_count
+    end
+
+  end # validations
+
+  describe "relations" do
+  end # relations
 end
