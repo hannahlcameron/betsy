cd975424a83c10c4e7e36eb6c66bc0fde2a0c467
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-19 14:35:36 -0700

    Update Review model to include description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419172655) do
+ActiveRecord::Schema.define(version: 20180419213238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,9 +31,10 @@ ActiveRecord::Schema.define(version: 20180419172655) do
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
     t.integer "product_id"
-    t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -72,9 +73,11 @@ ActiveRecord::Schema.define(version: 20180419172655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "product_id"
+    t.text "description"
     t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
+  add_foreign_key "order_items", "orders"
   add_foreign_key "products", "merchants"
   add_foreign_key "reviews", "products"
 end
