e7a2a15463af5104435ddce3a19c1e8353a3d76d
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-19 21:20:59 -0700

    all model tests passing

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,9 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  validates :order_id, presence: true
+  validates :product_id, presence: true
+  validates :quantity, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
+  validates_numericality_of :quantity, only_integer: true
 end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -5,35 +5,80 @@ describe OrderItem do
     it 'is valid with a index_reviews_on_product_id' do
 
       product_id = Product.first.id
-      order_item = OrderItem.new
-      order_item.valid?.must_equal false
-      order_item.product_id.push(product_id)
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 5)
+
+      order_item.valid?.must_equal true
 
     end
 
     it 'is invalid without a product_id' do
 
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 24)
+
+      order_item.valid?.must_equal true
+
+      order_item.product_id = nil
+
+      order_item.valid?.must_equal false
 
     end
 
     it 'is valid with an order_id' do
 
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 9)
+
+      order_item.valid?.must_equal true
     end
 
-    it 'is valid without an order_id' do
+    it 'is invalid without an order_id' do
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 2)
 
+      order_item.valid?.must_equal true
+
+      order_item.order_id = nil
+
+      order_item.valid?.must_equal false
     end
 
     it 'is valid with a quantity greater than zero' do
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 3)
+
+      order_item.valid?.must_equal true
 
     end
 
     it 'is invalid when the quantity is not an integer' do
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 3)
+
+      order_item.valid?.must_equal true
+
+      order_item.quantity = "potatoes"
+
+      order_item.valid?.must_equal false
 
     end
 
     it 'is invalid when the quantity is zero' do
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 3)
 
+      order_item.valid?.must_equal true
+
+      order_item.quantity = 0
+
+      order_item.valid?.must_equal false
     end
 
   end
