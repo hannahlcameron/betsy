0d8564dc88ba088909a93360ad878ca4f301d2c0
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-20 00:00:43 -0700

    added in some controller tests and placeholder html; fixed typo in routes

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -1,4 +1,48 @@
 class OrderitemsController < ApplicationController
+  before_action :find_order_item, only: [:edit, :update, :destroy]
 
-  
+  def index
+
+  end
+
+  def new
+    @orderitem = OrderItem.new
+  end
+
+  def create
+    @orderitem = OrderItem.new(order_item_params)
+
+    if @orderitem.save
+      flash[:success] = "Item added successfully!"
+      redirect_to products_path
+    else
+      flash.now[:failure] = "Oops! Something went wrong and we couldn't add this item."
+      render "products/show", status: :bad_request
+  end
+
+  def edit; end
+
+  def update
+    @order_item.assign_attributes(order_item_params)
+
+    if @orderitem.save
+      flash[:success] = "Item added successfully!"
+      redirect_to products_path
+    else
+      flash.now[:failure] = "Oops! Something went wrong and we couldn't add this item."
+      render "products/show", status: :bad_request
+
+  end
+
+  def show; end
+
+  def destroy
+  end
+
+  private
+
+  def order_item_params
+    params.require(:orderitem).permit(:order_id, :product_id, :quantity)
+  end
+
 end
--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -1,7 +1,102 @@
 require "test_helper"
 
 describe OrderitemsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
+  describe 'new' do
+    it 'responds with success' do
+      get new_orderitem_path
+      must_respond_with :success
+    end
+  end
+
+  # describe 'create' do
+  #   it 'can add a valid orderitem' do
+  #     oi_data = {
+  #       product_id: Product.first.id,
+  #       order_id: Order.first.id,
+  #       quantity: 1
+  #     }
+  #
+  #     old_oi_count = OrderItem.count
+  #
+  #     OrderItem.new(oi_data).must_be :valid?
+  #
+  #     post orderitems_path, params: {orderitem: oi_data}
+  #
+  #     must_respond_with :redirect
+  #     must_redirect_to products_path
+  #
+  #     OrderItem.count.must_equal old_oi_count + 1
+  #
+  #   end
+  #
+  #   it "won't add an invalid orderitem" do
+  #     oi_data = {
+  #       product_id: Product.first.id,
+  #       quantity: 1
+  #     }
+  #
+  #     old_oi_count = OrderItem.count
+  #
+  #     OrderItem.new(oi_data).wont_be :valid?
+  #
+  #     post orderitems_path, params: {orderitem: oi_data}
+  #
+  #     must_respond_with :bad_request
+  #
+  #     OrderItem.count.must_equal old_oi_count
+  #   end
   # end
+
+  describe 'edit' do
+    it 'responds with success' do
+      get edit_orderitem_path(OrderItem.last)
+      must_respond_with :success
+    end
+
+    it 'sends not_found if order_item does not exist' do
+      orderitem_id = OrderItem.last.id + 1
+      get orderitem_path(orderitem_id)
+      must_respond_with :not_found
+    end
+  end
+
+  describe 'update' do
+    it 'updates an existing orderitem with valid data' do
+      test_io = OrderItem.first
+      oi_data = test_io.attributes
+      oi_data[:quantity] = 2
+
+      test_io.assign_attributes(oi_Data)
+      test_io.must_be :valid
+
+      patch orderitem_path(test_io), params: { orderitem: io_data }
+
+      must_redirect_to products_path
+
+      test_io.reload
+      test_io.quantity.must_equal io_data[:quantity]
+
+    end
+
+    it 'sends bad_request for invalid data' do
+      test_io = OrderItem.first
+      oi_data = test_io.attributes
+      oi_data[:quantity] = 20000
+
+      test_io.assign_attributes(oi_Data)
+      test_io.wont_be :valid
+
+      patch orderitem_path(test_io), params: { orderitem: io_data }
+
+      must_respond_with :bad_request
+    end
+
+    it 'sends not_found for orderitem that dne' do
+      io_id = OrderItem.last.id + 1
+      patch orderitem_path(io_id)
+
+      must_respond_with :not_found
+    end
+
+  end
 end
