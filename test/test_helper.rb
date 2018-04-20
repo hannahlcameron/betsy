8277c6709dd59539e17691c7fd2a2ab158d919df
Tor Shimizu <torshimizu@gmail.com>
2018-04-20 11:28:30 -0700

    added simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/' # for rspec
+  add_filter '/test/' # for minitest
+end
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
