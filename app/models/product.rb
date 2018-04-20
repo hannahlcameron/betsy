c0e888128a14fb69eb1717e638a796933af15ef7
Tor Shimizu <torshimizu@gmail.com>
2018-04-19 23:41:29 -0700

    added tests for product and required validations, added a product controller, restful method skeletons and view files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  # can be created without a category?
   has_and_belongs_to_many :categories, join_table: :products_categories
   has_many :reviews
   belongs_to :merchant
@@ -6,5 +7,5 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_items
 
   validates :name, presence: true, uniqueness: true
-  # validates :price, presence: true, numericality: {greater_than: 0, only_number: true}
+  validates :price, presence: true, numericality: {greater_than: 0}
 end
