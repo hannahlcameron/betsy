ce2cb6423cbf0fb733957f1bad12ba38f8ec93de
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-21 10:47:27 -0700

    converted product.stock to integer from string and modified orderitem validation to match

--- db/migrate/20180421173034_change_product_stock_to_integer.rb
+++ db/migrate/20180421173034_change_product_stock_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeProductStockToInteger < ActiveRecord::Migration[5.1]
   def change
-    change_column :products, :stock, :integer
+    change_column :products, :stock, :integer, using: 'stock::integer'
   end
 end
