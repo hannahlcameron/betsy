6999584ee5629606db0c5ed3eca5e22be9731cdc
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-21 13:51:03 -0700

    started products show view; added order/viewcart route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :orderitems do
-  end
-
-  resources :products do
-  end
+  resources :orderitems
+  resources :products
+  resources :order
+  get 'order/viewcart', to: 'orders#viewcart', as: viewcart
 
 end
