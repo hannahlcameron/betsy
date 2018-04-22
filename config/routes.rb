c12b61a119ab4b15b2130d28a0d07282d193346e
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-22 13:32:58 -0700

    added in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :orderitems do
+  resources :orderitems
+  resources :products
+  resources :merchants do
+    resources :products, only: [:index, :new]
   end
-
-  resources :products do
-  end
+  resources :order
+  get '/viewcart', to: 'order#viewcart', as: 'viewcart'
 
 end
