9d25f3bd60126ba92cbb92290ad6baf655c399f0
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-23 16:04:52 -0700

    Copy in OrderItem validation tests from orderitem_model_testing branch

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,11 +2,16 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
+  validates :order_id, presence: true
+  validates :product_id, presence: true
+  validates :quantity, presence: true, :numericality => { :greater_than_or_equal_to => 1, only_integer: true }
   validate :available_quantity
 
   def available_quantity
-    unless product.stock >= quantity
-      errors[:quantity] << 'Not enough stock'
+    if product
+      unless product.stock >= quantity
+        errors[:quantity] << 'Not enough stock'
+      end
     end
   end
 
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -3,6 +3,91 @@ require 'pry'
 
 describe OrderItem do
 
+  describe 'validations' do
+    it 'is valid with a index_reviews_on_product_id' do
+
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 1)
+      order_item.valid?.must_equal true
+
+    end
+
+    it 'is invalid without a product_id' do
+
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(order_id: order_id, quantity: 1)
+
+      order_item.wont_be :valid?
+
+    end
+
+    it 'is valid with an order_id' do
+
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 1)
+
+      order_item.valid?.must_equal true
+    end
+
+    it 'is invalid without an order_id' do
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 2)
+
+      order_item.valid?.must_equal true
+
+      order_item.order_id = nil
+
+      order_item.valid?.must_equal false
+    end
+
+    it 'is valid with a quantity greater than zero' do
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 3)
+
+      order_item.valid?.must_equal true
+
+    end
+
+    it 'is invalid when the quantity is not an integer' do
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 3)
+
+      order_item.valid?.must_equal true
+
+      order_item.quantity = "potatoes"
+
+      order_item.valid?.must_equal false
+
+    end
+
+    it 'is invalid when the quantity is zero' do
+      product_id = Product.first.id
+      order_id = Order.first.id
+      order_item = OrderItem.new(product_id: product_id, order_id: order_id, quantity: 3)
+
+      order_item.valid?.must_equal true
+
+      order_item.quantity = 0
+
+      order_item.valid?.must_equal false
+    end
+
+    it 'is invalid with a quantity greater than the product stock' do
+      product = Product.first
+      order = Order.first
+      stock = product.stock
+      order_item = OrderItem.new(product_id: product.id, order_id: order.id, quantity: stock + 1)
+      order_item.wont_be :valid?
+    end
+
+  end # validations
+
   describe "business logic" do
 
     describe "subtotal" do
