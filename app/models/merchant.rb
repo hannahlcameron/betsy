d3010b7e554a01525b6451eeea7c38683390c698
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-23 12:06:27 -0700

    Write and test merchant_order_items in Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,26 @@ class Merchant < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
 
+  def merchant_order_items
+
+    orders_and_items = {}
+    orders = self.orders
+    order_ids = orders.map { |order| order.id }
+    order_ids.each { |order_id| orders_and_items[order_id] = [] }
+
+    order_items = []
+    self.products.each { |product|
+      product.order_items.each { |order_item|
+        order_items << order_item
+      }
+    }
+
+    order_items.each { |order_item|
+      orders_and_items[order_item.order_id] << order_item
+    }
+
+    return orders_and_items
+
+  end # merchant_order_items
+
 end
