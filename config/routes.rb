364f1be94f05dbb166620ae347e0a8b5612a6498
Tor Shimizu <torshimizu@gmail.com>
2018-04-22 22:54:31 -0700

    merged routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-<<<<<<< HEAD
   root 'products#index'
-
-=======
-  root 'orders#index'
 
   resources :orders
   resources :products
->>>>>>> master
   resources :orderitems do
   end
 
