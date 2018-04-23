50558bb22efab75db2b3f8fc0149f1dfd262a1f8
Tor Shimizu <torshimizu@gmail.com>
2018-04-23 15:22:57 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,15 @@ Rails.application.routes.draw do
   root 'products#index'
 
   resources :orders
-  resources :merchants do
+  resources :merchants, except: [:new, :create] do
     resources :orders, only: [:index]
     resources :products, except: [:show]
   end
 
-  get "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
-  delete "/logout", to: "sessions#destroy", as: "logout"
   resources :orderitems do
   end
 
   resources :category, only: [:create]
-
-  resources :merchants, except: [:new, :create] do
-    resources :products, except: [:show]
-  end
-
   resources :products, only: [:index, :show] do
   end
   get '/:category', to: 'products#index', as: 'category'
@@ -28,5 +21,4 @@ Rails.application.routes.draw do
 
   get "/auth/:provider/callback", to: "sessions#create", as: 'auth_callback_path'
   delete "/logout", to: "sessions#destroy", as: "logout"
-
 end
