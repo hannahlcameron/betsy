395e9e3e8915ce6c79c7c3586d60ed02d4f226f0
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-24 12:04:36 -0700

    Add status column to OrderItem db table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423172432) do
+ActiveRecord::Schema.define(version: 20180424190055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,12 @@ ActiveRecord::Schema.define(version: 20180423172432) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "product_id"
     t.bigint "order_id"
+    t.string "status"
     t.index ["order_id"], name: "index_order_items_on_order_id"
-    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 20180423172432) do
   end
 
   add_foreign_key "order_items", "orders"
-  add_foreign_key "order_items", "products"
   add_foreign_key "products", "merchants"
   add_foreign_key "reviews", "products"
 end
