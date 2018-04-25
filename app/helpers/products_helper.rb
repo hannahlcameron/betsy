87df630a04ad4e723625b6c056748f80466697f8
Tor Shimizu <torshimizu@gmail.com>
2018-04-25 10:58:29 -0700

    added reviews form, added conversion of rating to lightening bolts to helper, changed product cards to be in a flexbox

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+  def star_rating(rating)
+    stars = ""
+    rating.times do
+      stars += "#{ image_tag 'lightening-bolt.png', alt: 'lightening bolt', width: 20, height: 20}"
+    end
+    (5 - rating).times do
+      stars += "#{ image_tag 'grey-lightening-bolt.jpg', alt: 'grey lightening bolt', width: 20, height: 20}"
+    end
+    return (stars).html_safe
+  end
 end
