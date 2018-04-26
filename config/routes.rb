a0bc268c2808505937b3d3a15b7a9e83397466d1
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-26 16:45:55 -0700

    added back in merchants products index path

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :merchants, except: [:new, :create] do
     resources :orders, only: [:index]
-    resources :products, except: [:show, :index]
+    resources :products, except: [:show]
     get '/manage_products', to: 'merchants#manage_products', as: 'manage_products'
   end
 
