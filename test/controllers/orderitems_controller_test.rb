d5d2e930664715fa51d847fcf9c0a7900f5ad4aa
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-26 16:32:41 -0700

    fixed typo in test; removed unneccesary test

--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -103,7 +103,7 @@ describe OrderitemsController do
       delete orderitem_path(io_id)
 
       must_respond_with :redirect
-      must_redirect_to "order/show"
+      must_redirect_to "/order/show"
 
       OrderItem.count.must_equal old_oi_count - 1
       OrderItem.find_by(id: io_id).must_be_nil
