7cf038467936d10c7c8a77c779103d6bd7d4fc0f
Tor Shimizu <torshimizu@gmail.com>
2018-04-26 00:41:25 -0700

    updated Category.categories_with_works method to alphabetize, attempted to write tests for it, updated merchant manage products show page

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def self.categories_with_works
-    return Category.find_by_sql('SELECT DISTINCT categories.name FROM categories INNER JOIN products_categories ON categories.id = products_categories.category_id INNER JOIN products ON products_categories.category_id = products.id')
+    return Category.find_by_sql('SELECT DISTINCT categories.name FROM categories INNER JOIN products_categories ON categories.id = products_categories.category_id INNER JOIN products ON products_categories.category_id = products.id ORDER BY categories.name')
   end
 
   def self.fix_category(params)
